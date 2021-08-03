60db5ffd6e4540443af3a3dff651727299f5df20
nothing-git-hub <aswinbabu798@gmail.com>
2021-08-03 23:07:38 +0530

    yep

---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views 
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('<slug:c_slug>/',views.home,name='prod_cat'),
+    path('<slug:c_slug>/<slug:product_slug>',views.prodDetails,name='details'),
+    path('search',views.searching,name='search'),
+    path('login',views.login,name='login'),
+    path('register',views.register,name='register')
+]
